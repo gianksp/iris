030c2d991084f765b7743892833c9ac1724bf5b8
Giancarlo Sanchez <gianksp@gmail.com>
2012-12-30 22:20:51 -0430

    Integration webservice-rivescript engine

---
+++ com.skynet.iris.base/src/test/java/com/skynet/iris/base/bean/XmppClientBeanTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.skynet.iris.base.bean;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author gianksp
+ */
+public class XmppClientBeanTest extends TestCase {
+    
+    public XmppClientBeanTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+       // XmppClientBean xmppClientBean = new XmppClientBean("Giank", "localhost", "iris", "123");
+    }
+}
